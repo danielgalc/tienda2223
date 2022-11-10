806c299249586ecd6949c5dcb1ab28433e0896dc
danielgalc <daniel.gallego@iesdonana.org>
2022-11-10 20:25:46 +0100

    Tailwind CSS

--- public/admin/index.php
+++ public/admin/index.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/output.css" rel="stylesheet">
     <title>Listado de artículos</title>
 </head>
 
 <body>
     <?php
-    require 'auxiliar.php';
+    require '../../src/admin-auxiliar.php';
+    require '../../src/auxiliar.php';
 
     $codigo = $_GET['codigo'];
     /* $descripcion = $_GET['descripcion']; */
