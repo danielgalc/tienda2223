9db57f5ca67bb76b7df1c86592f6046837b2dc20
danielgalc <daniel.gallego@iesdonana.org>
2022-11-10 21:04:40 +0100

    Flowbite - Cambios mínimos

--- public/admin/index.php
+++ public/admin/index.php
@@ -22,12 +22,11 @@
     <div>
         <form action="" method="get">
             <fieldset>
-                <legend>Criterios de búsqueda</legend>
                 <p>
-                    <label>
+                    <label class="block mb-2 text-sm font-bold text-gray-900 dark:text-gray-300">
                         Código:
                         <input type="text"  name="codigo"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-25   p-2.5 
                         dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     </label>
                 </p>
